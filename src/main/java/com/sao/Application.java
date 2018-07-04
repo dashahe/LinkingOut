103aa2e3fa52d184377f7c85d5e02576df6b979a
xiebei1108 <xiebei1108@outlook.com>
2018-07-04 20:41:58 +0800

    [ADD]README

--- src/main/java/com/sao/Application.java
+++ src/main/java/com/sao/Application.java
@@ -23,9 +23,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class ThymeleafApplication {
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(ThymeleafApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
