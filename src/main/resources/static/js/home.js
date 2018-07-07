95c4eb39b5fec382986840e34f802e2484bb1cd9
Jing <985035177@qq.com>
2018-07-07 17:45:52 +0800

    [ADD]

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -13,7 +13,7 @@ function message() {
     else {
         xmlhttp = new ActiveXObject(" ");
     }
-    xmlhttp.onreadystatechange = function (ev) {
+    xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             alert("发送成功");
         }
@@ -21,6 +21,15 @@ function message() {
             alert("发送失败");
         }
     }
-    xmlhttp.open("get", "/", true);
+    xmlhttp.open("post", "/", true);
     xmlhttp.send();
+}
+
+function getUrl() {
+    var uid = document.getElementById("uid").value;
+    document.getElementById("form").action = "/" + uid + "/edit";
+}
+
+function goto(url) {
+    window.open(url);
 }
--- src/main/resources/static/js/url.js
+++ src/main/resources/static/js/url.js
@@ -2,3 +2,7 @@ function getUrl() {
     var uid = document.getElementById("uid").value;
     document.getElementById("form").action = "/" + uid + "/edit";
 }
+
+function goto(url) {
+    window.open(url);
+}
