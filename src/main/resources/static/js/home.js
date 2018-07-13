51d8d85af2f95b232ff619e666a9d3be24fd8bb4
Jing <985035177@qq.com>
2018-07-13 17:57:35 +0800

    [ADD STAR]

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -21,3 +21,26 @@ function message(id) {
     xmlhttp.open("post", "/", true);
     xmlhttp.send(content);
 }
+
+
+function star() {
+    var xmlhttp;
+    if(window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            xmlhttp.open("get", "/star", true);
+            xmlhttp.send();
+        }
+
+    }
+    var star = new FormData();
+    star.append("aid", document.getElementById(id).value);
+    xmlhttp.open("post", "/star", true);
+    xmlhttp.send(star);
+
+
+}
