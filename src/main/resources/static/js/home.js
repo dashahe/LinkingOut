b3968a0e3440381aaf9af0816dd08f5d5bca4d53
Jing <985035177@qq.com>
2018-07-13 11:49:32 +0800

    [ADD]

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,28 +1,12 @@
 
 
-    switch (c) {
-        case 1:
-            document.getElementsByName("content")[0].innerHTML = "#Daily# ";
-            break;
-        case 2:
-            document.getElementsByName("content")[0].innerHTML = "#Second Hand# ";
-            break;
-        case 3:
-            document.getElementsByName("content")[0].innerHTML = "#Hire# ";
-            break;
-        case 4:
-            document.getElementsByName("content")[0].innerHTML = "#Part-time Job# ";
-            break;
-
-    }
-}
 
 function message(id) {
     var xmlhttp;
     if(window.XMLHttpRequest) {
         xmlhttp = new XMLHttpRequest();
     } else {
-        xmlhttp = new ActiveXObject(" ");
+        xmlhttp = new ActiveXObject("");
     }
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -31,8 +15,7 @@ function message(id) {
     };
     var content = new FormData();
     content.append("content", document.getElementsByName("content")[0].value);
-    var type = document.getElementsByClassName("fade")[0].id;
-    content.append("type", type);
+    content.append("type", id);
     xmlhttp.open("post", "/", true);
     xmlhttp.send(content);
 }
